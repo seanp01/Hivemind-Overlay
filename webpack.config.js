b674a6fe727f4da94c152931020456d2966a88f3
Sean Parker <parkermsean@gmail.com>
2025-06-10 12:40:43 -0400

    Hivemind Overlay v1.1.0 - Implemented webpack and built a webpack.config.js. Also implemented node and added a package.json.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: {
+    content: path.join(__dirname, 'extension', 'content.js'),
+    background: path.join(__dirname, 'extension', 'background.js')
+  },
+  output: {
+    path: path.join(__dirname, 'extension'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["@babel/env", {
+              "targets": {
+                "browsers": "last 2 chrome versions"
+              }
+            }]
+          ]
+        }
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devtool: 'source-map'
+};
